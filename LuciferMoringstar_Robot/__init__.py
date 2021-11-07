9b8e73b0770827588a2ccfedf42c627d464895ed
BANDU-OF-TG <87604582+jithin123455@users.noreply.github.com>
2021-11-07 19:07:35 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -12,24 +12,8 @@ from .Channel import (
 
 HELP = """
 <b><u>Basic Commads</u></b>
-
-➥ /start : check if am alive or dead
-➥ /about : About Me
-➥ /stats user Status
-
-<b><u>Bot Owner Only</u></b>
-
-➥ /broadcast Replay Any Message or Media
-➥ /ban_user  click ban_user more info
-➥ /unban_user click unban_user more info
-➥ /banned_users Banned User Details
-➥ /total How Many Files Added In Database
-➥ /logger  Get Logs
-➥ /delete Delete File From Database
-
-<b>[➡️How To Add Old Files For database](https://youtu.be/mtu5hgW-l_o)
-
-@Mo_Tech_YT></b>
+PODA MYRE.....ITHU ENTE GROUPILE PATTU
+@mr_bandu></b>
 """
 
 ABOUT = """
@@ -37,11 +21,11 @@ ABOUT = """
     
 ➥ Credits : Subin
 
-➥ Editing : [Muhammed RK](t.me/Mrk_YT)
+➥ Editing : [BANDU](t.me/bandu_of_tg)
 
 ➥ Language : Python3
 
 ➥ Library : Pyrogram Asyncio 1.13.0
 
-➥ Source Code : [Click Me](https://github.com/PR0FESS0R-99/LuciferMoringstar_Robot)
+➥ FOR MOVIES : [JOIN NOW](https://t.me/cinemacollectiongroup)
 """
